714104cf08902a43c42531c3c026d8f50d44946b
sdldev <sidolapakdev@gmail.com>
2024-08-06 15:15:40 +0700

    update formater

--- src/components/Attendence.jsx
+++ src/components/Attendence.jsx
@@ -66,49 +66,37 @@ const Form = () => {
       </div>
       {responseData && responseData.success && (
 
-        <div className="py-3 flex items-center justify-center flex-auto flex-col">
-          <div
-            className="relative flex w-full max-w-[48rem] flex-row rounded-xl text-gray-700 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]"
-          >
-            <div
-              className="relative m-0 w-2/6 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700"
-            >
-              <img
-                src={responseData.data.image}
-                alt="image"
-                className="h-full w-full object-cover"
-              />
-            </div>
-            <div className="p-6">
-              <h6
-                className="mb-4 block text-base font-semibold uppercase leading-relaxed tracking-normal text-sky-200 antialiased"
-              >
-                {responseData.data.code} {responseData.data.nisn}  {responseData.data.kelas}
-              </h6>
-              <div
-                className="mb-2 block text-lime-200 text-3xl font-semibold antialiased"
-              >
-                {responseData.data.name}
-              </div>
-              <p
-                className="py-3 block text-sky-400 antialiased"
-              >
-                Masuk: {responseData.data.timein}
-              </p>
-              <p
-                className="mb-8 block text-sky-400 antialiased"
-              >
-                Keluar: {responseData.data.timeout}
-              </p>
-              <div
-                className="items-center justify-center text-2xl flex-auto text-red-200 flex flex-col"
-              >
-                {responseData.message.text}
-              </div>
-            </div>
-          </div>
-
-        </div>
+<div className="py-3 flex items-center justify-center flex-auto flex-col">  
+  <div className="relative flex w-full max-w-[48rem] flex-row rounded-xl text-gray-700 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">  
+    <div className="relative m-0 w-2/6 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">  
+      <img  
+        src={responseData.data.image}  
+        alt="image"  
+        className="h-full w-full object-cover"  
+      />  
+    </div>  
+    <div className="p-6">  
+      <h6 className="mb-4 block text-base font-semibold uppercase leading-relaxed tracking-normal text-sky-200 antialiased">  
+        {responseData.data.code} - {responseData.data.nisn} - {responseData.data.kelas}  
+      </h6>  
+      <div className="mb-2 block text-lime-200 text-3xl font-semibold antialiased">  
+        {responseData.data.name}  
+      </div>  
+      {/* Membungkus elemen paragraf dengan div untuk menggunakan Flexbox */}  
+      <div className="flex text-base font-semibold flex-col md:flex-row items-center justify-between py-4"> {/* space-x-4 memberikan jarak antara kolom */}  
+        <span className="flex py-4 text-blue-200">  
+          {responseData.data.timein}  
+        </span>  
+        <span className="py-3 mb-3 flex justify-end space-x-2 text-red-400 antialiased">  
+          {responseData.data.timeout}  
+        </span>  
+      </div>  
+      <div className="items-center justify-center text-2xl flex-auto text-sky-200 flex flex-col">  
+        {responseData.message.text}  
+      </div>  
+    </div>  
+  </div>  
+</div>
       )}
     </div>
   );
